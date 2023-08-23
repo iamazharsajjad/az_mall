12d78e818725dd19571446f8920473977b811c63
iamazharsajjad <iamazharsajjad@gmail.com>
2023-08-23 18:39:30 +0500

    theme integrated!

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,22 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+
+
+import './vendor/jquery-2.1.0.min'
+
+import './vendor/popper'
+import './vendor/bootstrap.min'
+
+import './vendor/owl-carousel'
+import './vendor/accordions'
+import './vendor/datepicker'
+import './vendor/scrollreveal.min'
+import './vendor/waypoints.min'
+import './vendor/jquery.counterup.min'
+import './vendor/imgfix.min' 
+import './vendor/slick' 
+import './vendor/lightbox' 
+import './vendor/isotope' 
+
+import './vendor/custom'
